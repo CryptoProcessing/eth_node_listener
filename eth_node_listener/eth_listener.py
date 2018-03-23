009b0998daf7453528ca3d0fc204c4c8f741ce0f
ice1snice <mitin@phystech.edu>
2018-03-23 16:24:50 +0300

    Geth node listen via websocket, get date from web3 and send it to message queue

--- eth_node_listener/eth_listener.py
+++ eth_node_listener/eth_listener.py
@@ -1,5 +1,4 @@
-import websocket
-import json
+import websocket, json, pika, time
 import threading
 
 from web3 import Web3, HTTPProvider
@@ -25,13 +24,28 @@ class ETHListener:
     def __init__(
             self, app,
             ws_addr='ws://localhost:8546',
-            rpc_addr='http://127.0.0.1:8545'):
+            rpc_addr='http://127.0.0.1:8545',
+            amqp_connection_params=pika.ConnectionParameters(host='localhost'),
+            queue_name='eth_node_events'):
         self._app = app
         self._ws_addr = ws_addr
         self._web3 = Web3(HTTPProvider(rpc_addr))
 
+        self._amqp_connection = pika.BlockingConnection(amqp_connection_params)
+        self._mq_queue_name = queue_name
+
+        self._channel = self._amqp_connection.channel()
+        self._channel.queue_declare(queue=self._mq_queue_name)
+
+    def _publish_to_amqp(
+            self, body='', 
+            exchange='', routing_key='eth_node_events'):
+        if not routing_key:
+            routing_key = self._mq_queue_name
+        self._channel.basic_publish(
+            exchange=exchange, routing_key=routing_key, body=body)
+
     def _on_message(self, ws, message):
-        print(message)
         j = json.loads(message)
         if 'id' in j:
             sub_id = j['result']
@@ -46,6 +60,7 @@ class ETHListener:
         return
 
     def _on_close(self, ws):
+        self._amqp_connection.close()
         return
 
     def _on_open(self, ws):
@@ -80,11 +95,23 @@ class ETHListener:
             return self._process_new_tx(res)
 
     def _process_new_head(self, new_head):
-        block = self._web3.eth.getBlock(new_head['hash'], full_transactions=True)
-        print('new head')
-        return
+        block = self._web3.eth.getBlock(
+            new_head['hash'], full_transactions=True)
+        block_as_dict = dict(block)
+        tx_list = []
+        for tx in block['transactions']:
+            tx_list.append(dict(tx))
+        block_as_dict['transactions'] = tx_list
+        mq_msg = {
+            'ethNodeEvent': 'newHead',
+            'data': block_as_dict,
+        }
+        self._publish_to_amqp(body=json.dumps(mq_msg))
 
     def _process_new_tx(self, new_tx):
         tx = self._web3.eth.getTransaction(new_tx)
-        print('new tx')
-        return
+        mq_msg = {
+            'ethNodeEvent': 'newPendingTransaction',
+            'data': dict(tx),
+        }
+        self._publish_to_amqp(body=json.dumps(mq_msg))
